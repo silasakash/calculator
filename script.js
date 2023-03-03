24ffbea8c0ee0613f38d2171384ad6e6f17c6199
silasakash <akashsilas1@gmail.com>
2023-03-03 23:41:24 +0530

    Add functionalities to number buttons

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// buttons
+const acButton = document.getElementById("acButton");
+const negateButton = document.getElementById("negateButton");
+const percentageButton = document.getElementById("percentageButton");
+const divideButton = document.getElementById("divideButton");
+const sevenButton = document.getElementById("sevenButton");
+const eightButton = document.getElementById("eightButton");
+const nineButton = document.getElementById("nineButton");
+const multiplyButton = document.getElementById("multiplyButton");
+const fourButton = document.getElementById("fourButton");
+const fiveButton = document.getElementById("fiveButton");
+const sixButton = document.getElementById("sixButton");
+const subtractButton = document.getElementById("subtractButton");
+const oneButton = document.getElementById("oneButton");
+const twoButton = document.getElementById("twoButton");
+const threeButton = document.getElementById("threeButton");
+const addButton = document.getElementById("addButton");
+const zeroButton = document.getElementById("zeroButton");
+const dotButton = document.getElementById("dotButton");
+const equalsButton = document.getElementById("equalsButton");
+
+const result = document.getElementById("result");
+
 //operations
 function add(no1, no2) {
     return no1 + no2;
@@ -43,3 +66,79 @@ function operate(operator, operand1, operand2) {
         return percentage(operand1);
     }
 }
+
+acButton.addEventListener("click", function () {
+    result.textContent = '';
+});
+
+negateButton.addEventListener("click", function () {
+    result.textContent = - Number(result.textContent);
+});
+
+percentageButton.addEventListener("click", function () {
+    result.textContent = (Number(result.textContent))/100;
+});
+
+divideButton.addEventListener("click", function () {
+    result.textContent = - Number(result.textContent);
+});
+
+sevenButton.addEventListener("click", function () {
+    result.textContent = "7";
+});
+
+eightButton.addEventListener("click", function () {
+    result.textContent = "8";
+});
+
+nineButton.addEventListener("click", function () {
+    result.textContent = "9";
+});
+
+multiplyButton.addEventListener("click", function () {
+    result.textContent = - Number(result.textContent);
+});
+
+fourButton.addEventListener("click", function () {
+    result.textContent = "4";
+});
+
+fiveButton.addEventListener("click", function () {
+    result.textContent = "5";
+});
+
+sixButton.addEventListener("click", function () {
+    result.textContent = "6";
+});
+
+subtractButton.addEventListener("click", function () {
+    result.textContent = - Number(result.textContent);
+});
+
+oneButton.addEventListener("click", function () {
+    result.textContent = "1";
+});
+
+twoButton.addEventListener("click", function () {
+    result.textContent = "2";
+});
+
+threeButton.addEventListener("click", function () {
+    result.textContent = "3";
+});
+
+addButton.addEventListener("click", function () {
+    result.textContent = - Number(result.textContent);
+});
+
+zeroButton.addEventListener("click", function () {
+    result.textContent = "0";
+});
+
+dotButton.addEventListener("click", function () {
+    result.textContent = - Number(result.textContent);
+});
+
+equalsButton.addEventListener("click", function () {
+    result.textContent = - Number(result.textContent);
+});
