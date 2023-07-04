7893940d4c6830eec69ffeb8eda59e6c311c39b7
silasakash <akashsilas1@gmail.com>
2023-07-04 23:52:50 +0530

    Modify script for working with single digit numbers

--- script.js
+++ script.js
@@ -67,12 +67,20 @@ function operate(operator, operand1, operand2) {
     }
 }
 
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+
 acButton.addEventListener("click", function () {
-    result.textContent = '';
+    result.textContent = '0';
+    firstNumber = null;
+    secondNumber = null;
+    operator = null;
 });
 
 negateButton.addEventListener("click", function () {
     result.textContent = - Number(result.textContent);
+    firstNumber = - firstNumber;
 });
 
 percentageButton.addEventListener("click", function () {
@@ -80,58 +88,119 @@ percentageButton.addEventListener("click", function () {
 });
 
 divideButton.addEventListener("click", function () {
-    result.textContent = - Number(result.textContent);
+    operator = "/";
+
 });
 
 sevenButton.addEventListener("click", function () {
+    if (firstNumber == null) {
+        firstNumber = 7;
+    }
+    else {
+        secondNumber = 7;
+    }
     result.textContent = "7";
 });
 
 eightButton.addEventListener("click", function () {
+    if (firstNumber == null) {
+        firstNumber = 8;
+    }
+    else {
+        secondNumber = 8;
+    }
     result.textContent = "8";
 });
 
 nineButton.addEventListener("click", function () {
+    if (firstNumber == null) {
+        firstNumber = 9;
+    }
+    else {
+        secondNumber = 9;
+    }
     result.textContent = "9";
 });
 
 multiplyButton.addEventListener("click", function () {
-    result.textContent = - Number(result.textContent);
+    operator = "*";
 });
 
 fourButton.addEventListener("click", function () {
+    if (firstNumber == null) {
+        firstNumber = 4;
+    }
+    else {
+        secondNumber = 4;
+    }
     result.textContent = "4";
 });
 
 fiveButton.addEventListener("click", function () {
+    if (firstNumber == null) {
+        firstNumber = 5;
+    }
+    else {
+        secondNumber = 5;
+    }
     result.textContent = "5";
 });
 
 sixButton.addEventListener("click", function () {
+    if (firstNumber == null) {
+        firstNumber = 6;
+    }
+    else {
+        secondNumber = 6;
+    }
     result.textContent = "6";
 });
 
 subtractButton.addEventListener("click", function () {
-    result.textContent = - Number(result.textContent);
+    operator = "-";
 });
 
 oneButton.addEventListener("click", function () {
+    if (firstNumber == null) {
+        firstNumber = 1;
+    }
+    else {
+        secondNumber = 1;
+    }
     result.textContent = "1";
 });
 
 twoButton.addEventListener("click", function () {
+    if (firstNumber == null) {
+        firstNumber = 2;
+    }
+    else {
+        secondNumber = 2;
+    }
     result.textContent = "2";
 });
 
 threeButton.addEventListener("click", function () {
+    if (firstNumber == null) {
+        firstNumber = 3;
+    }
+    else {
+        secondNumber = 3;
+    }
     result.textContent = "3";
 });
 
 addButton.addEventListener("click", function () {
-    result.textContent = - Number(result.textContent);
+    operator = "+";
 });
 
 zeroButton.addEventListener("click", function () {
+    if (firstNumber == null) {
+        firstNumber = 7;
+    }
+    else {
+        secondNumber = 7;
+    }
     result.textContent = "0";
 });
 
@@ -140,5 +209,17 @@ dotButton.addEventListener("click", function () {
 });
 
 equalsButton.addEventListener("click", function () {
-    result.textContent = - Number(result.textContent);
+    if (firstNumber != null && secondNumber != null && operator != null) {
+        let output = operate(operator,firstNumber,secondNumber);
+        if (Number.isInteger(output)) {
+            result.textContent = output;
+        }
+        else {
+            result.textContent = output.toFixed(5);
+        }        
+        firstNumber = output;
+        firstNumber = null;
+        secondNumber = null;
+        operator = null;
+    }
 });
